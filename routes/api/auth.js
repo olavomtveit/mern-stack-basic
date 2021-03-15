533651147ba14a7b2d0eb57a1cc4c2a2b0d784e3
cameronmoodley <moodley.cameron@gmail.com>
2021-03-15 15:52:21 +0100

    added in redux to the project

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const { body, validationResult } = require("express-validator");
-const config = require("config");
-const auth = require("../../middleware/auth");
-const User = require("../../models/User");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { body, validationResult } = require('express-validator');
+const config = require('config');
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 
 /*
  * @route  POST api/auth
@@ -13,60 +13,60 @@ const User = require("../../models/User");
  * @access Private
  */
 router.post(
-  "/",
-  body("firstName").isLength({ min: 3 }),
-  body("email").isEmail(),
-  body("password").isLength({ min: 5 }),
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+	'/',
+	body('firstName').isLength({ min: 3 }),
+	body('email').isEmail(),
+	body('password').isLength({ min: 5 }),
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
 
-    const { firstName, email, password } = req.body;
+		const { firstName, email, password } = req.body;
 
-    try {
-      let user = await User.findOne({ email });
+		try {
+			let user = await User.findOne({ email });
 
-      if (user) {
-        return res
-          .status(400)
-          .json({ errors: [{ message: "User already exists" }] });
-      }
+			if (user) {
+				return res
+					.status(400)
+					.json({ errors: [{ message: 'User already exists' }] });
+			}
 
-      user = new User({
-        firstName: firstName,
-        email: email,
-        password: password,
-      });
+			user = new User({
+				firstName: firstName,
+				email: email,
+				password: password,
+			});
 
-      const salts = await bcrypt.genSalt(10);
+			const salts = await bcrypt.genSalt(10);
 
-      user.password = await bcrypt.hash(password, salts);
+			user.password = await bcrypt.hash(password, salts);
 
-      const payload = {
-        user: {
-          id: user.id,
-        },
-      };
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
 
-      jwt.sign(
-        payload,
-        config.get("secretToken"),
-        {
-          expiresIn: 36000,
-        },
-        (error, token) => {
-          if (error) throw error;
-          res.json({ token });
-        }
-      );
+			jwt.sign(
+				payload,
+				config.get('secretToken'),
+				{
+					expiresIn: 36000,
+				},
+				(error, token) => {
+					if (error) throw error;
+					res.json({ token });
+				}
+			);
 
-      await user.save();
-    } catch (error) {
-      res.status(500).send(error);
-    }
-  }
+			await user.save();
+		} catch (error) {
+			res.status(500).send(error);
+		}
+	}
 );
 
 module.exports = router;
