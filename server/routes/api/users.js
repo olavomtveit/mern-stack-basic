550d7651be34e9a858919fb701de25eaa54c5fea
olavomtveit <omtveit94@gmail.com>
2021-03-17 14:00:20 +0100

    updated user fetch

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -26,7 +26,7 @@ router.get("/", auth, async (req, res) => {
 router.get("/:id", auth, async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
-    res.json({ message: `Here is your user ${user}` });
+    res.json(user);
   } catch (error) {
     console.error(error);
     res.status(500).send({ message: "user not found" });
